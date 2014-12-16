7ae532825c2c5af0e8ffe4f8f7ba19b95ff8193b
Mark Pleskac <markpleskac@gmail.com>
2014-12-15 20:45:08 -0800

    Initial Commit

---
+++ DataMapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DataMapper
+//
+//  Created by Mark Pleskac on 12/15/14.
+//  Copyright (c) 2014 Mark Pleskac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
