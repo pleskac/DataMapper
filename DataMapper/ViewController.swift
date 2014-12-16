8c21c2b45895c2d744f6ce2291c1a69cb402d397
Mark Pleskac <markpleskac@gmail.com>
2014-12-15 21:35:07 -0800

    Got it working... that was easy

--- DataMapper/ViewController.swift
+++ DataMapper/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import Cocoa
+import Foundation
+import MapKit
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet var mapView: MKMapView!
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var points = getJsonCoordinates()
+        for point in points{
+            var annotation = MKPointAnnotation()
+            var l = CLLocationCoordinate2D(latitude: point["Latitude"] as Double, longitude: point["Longitude"] as Double)
+            annotation.setCoordinate(l)
+            annotation.title = point["Title"] as String
+            mapView.addAnnotation(annotation)
+        }
+        
+        mapView.showAnnotations(mapView.annotations, animated: true)
+
+    }
+    
+    func getJsonCoordinates() -> NSArray {
+        var url: NSURL = NSURL(string: "http://pleskac.org:8080/api/trip/id/11")!
+        
+        var request1: NSURLRequest = NSURLRequest(URL: url)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?>=nil
+        var error: NSErrorPointer = nil
+        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
+        var err: NSError
+        println(response)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        return jsonResult["Coordinates"] as NSArray;
+
     }
 
 
